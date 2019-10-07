91b666424fc07ecb2c899db361a05edcb8e8fa83
Kevin Toala <kevin.toala@outlook.com>
2019-10-07 16:10:51 -0500

    Paso-3
    - Creación del topic SNS
    - Publicación de la venta creada en el topic de SNS

--- src/Venta.js
+++ src/Venta.js
@@ -4,23 +4,44 @@ const AWS = require('aws-sdk');
 const uuidv4 = require('uuid/v4');
 
 const dynamoDBClient = new AWS.DynamoDB();
+const snsClient = new AWS.SNS();
 
 module.exports.crear = async event => {
   const body = JSON.parse(event.body);
 
-  const params = {
+  const venta = {
+    id: uuidv4(),
+    cliente: body.cliente,
+    total: Number(body.total)
+  };
+
+  await guardarVenta(venta);
+  await publicarVenta(venta);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(venta),
+  };
+};
+
+async function guardarVenta(venta) {
+  const dynamoDBParams = {
     TableName: process.env.dynamoDBPrefix + 'ventas',
     Item: {
-      'id': {S: uuidv4()},
-      'cliente': {S: body.cliente},
-      'total': {N: body.total + ''}
+      'id': {S: venta.id},
+      'cliente': {S: venta.cliente},
+      'total': {N: venta.total + ''}
     }
   };
 
-  const request = await dynamoDBClient.putItem(params).promise();
-  console.log(request);
-  return {
-    statusCode: 200,
-    body: JSON.stringify(params.Item),
+  await dynamoDBClient.putItem(dynamoDBParams).promise();
+}
+
+async function publicarVenta(venta) {
+  const snsParams = {
+    TopicArn: process.env.snsARN,
+    Message: JSON.stringify(venta)
   };
-};
+
+  await snsClient.publish(snsParams).promise();
+}
